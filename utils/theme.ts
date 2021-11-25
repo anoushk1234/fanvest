9ddd2dbe100be478f1039ba664ea6e1515cb519f
anoushk1234 <anoushka1828@gmail.com>
2021-11-25 23:49:40 +0530

    init

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,23 +1,17 @@
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
-
 // Version 1: Using objects
 const theme = extendTheme({
   styles: {
-    global: {
-      // styles for the `body`
+    global: (props: any) => ({
       body: {
-        bg: "gray.400",
-        color: "white",
+        fontFamily: "body",
+        color: mode("gray.800", "whiteAlpha.900")(props),
+        bgImage:
+          "url('https://res.cloudinary.com/dev-connect/image/upload/v1637862655/img/Landing_Page_oyifso.png')",
+        lineHeight: "base",
       },
-      // styles for the `a`
-      a: {
-        color: "teal.500",
-        _hover: {
-          textDecoration: "underline",
-        },
-      },
-    },
+    }),
   },
 });
 
