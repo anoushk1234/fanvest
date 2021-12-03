8552a5a37cbd8d97cc371c8fc92d62c5c965661d
anoushk1234 <anoushka1828@gmail.com>
2021-12-03 21:54:03 +0530

    roadmap

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -13,84 +13,66 @@ import {
   Heading,
   IconButton,
   Stack,
+  Checkbox,
   Text,
 } from "@chakra-ui/react";
-// import "react-step-progress-bar/styles.css";
-// import { ProgressBar, Step } from "react-step-progress-bar";
+
 const Roadmap: NextPage = () => {
+  const its = ["Launch of v1.0", "Launch of alpha", "Fanvest Token"];
   return (
-    <>
-      {/* <style jsx>{`
-        .transitionStep {
-          filter: grayscale(1);
-        }
+    <Flex
+      flexDir="column"
+      py={10}
+      h="100%"
+      // border="1px solid white"
+      mx={{ base: "2rem", md: "6rem" }}
+    >
+      <Head>
+        <title>Explore</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
 
-        .transitionStep.accomplished {
-          filter: grayscale(0);
-        }
-      `}</style>
-
-      <ProgressBar
-        filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
-        percent={75}
-      >
-        <Step transition="scale">
-          {({ accomplished, index }: any) => (
-            <div
-              className={`transitionStep ${
-                accomplished ? "accomplished" : null
-              }`}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="//db.onlinewebfonts.com/c/9ecec61376083fd290b75d94fdaca380?family=Baskerville+Old+Face"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
+      {its.map((it, i) => {
+        return (
+          <Flex
+            key={i}
+            justifyContent="flex-start"
+            align="center"
+            // border="1px solid white"
+            mb={10}
+            ml={20}
+          >
+            <Image
+              src="https://res.cloudinary.com/dev-connect/image/upload/v1638547035/img/astrisk_grey_xlsyzx.svg"
+              alt="astrisk"
+              width={["10%", "10%", "10%", "10%"]}
+              height={["10%", "10%", "10%", "10%"]}
+            />
+            <Heading
+              as="h1"
+              fontSize={{ base: "3xl", md: "4rem" }}
+              px={["10%", "10%", "10%", "10%"]}
+              fontWeight="bold"
+              fontFamily="Baskerville Old Face"
+              noOfLines={1}
+              textAlign="left"
             >
-              🌑
-            </div>
-          )}
-        </Step>
-        <Step transition="scale">
-          {({ accomplished, index }: any) => (
-            <div
-              className={`transitionStep ${
-                accomplished ? "accomplished" : null
-              }`}
-            >
-              🌒
-            </div>
-          )}
-        </Step>
-        <Step transition="scale">
-          {({ accomplished, index }: any) => (
-            <div
-              className={`transitionStep ${
-                accomplished ? "accomplished" : null
-              }`}
-            >
-              🌓
-            </div>
-          )}
-        </Step>
-        <Step transition="scale">
-          {({ accomplished, index }: any) => (
-            <div
-              className={`transitionStep ${
-                accomplished ? "accomplished" : null
-              }`}
-            >
-              🌔
-            </div>
-          )}
-        </Step>
-        <Step transition="scale">
-          {({ accomplished, index }: any) => (
-            <div
-              className={`transitionStep ${
-                accomplished ? "accomplished" : null
-              }`}
-            >
-              🌕
-            </div>
-          )}
-        </Step>
-      </ProgressBar> */}
-    </>
+              {it}
+            </Heading>
+          </Flex>
+        );
+      })}
+    </Flex>
   );
 };
 
