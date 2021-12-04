62c9f8f4898aae541596066b96bd66a8b4741567
Anoushk Kharangate <32778608+anoushk1234@users.noreply.github.com>
2021-12-04 10:54:15 +0530

    og

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,18 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>Fanvest</title>
-        <meta name="description" content="Fanvest" />
+        <meta name="description" content="Fund your film with crypto" />
         <link rel="icon" href="/favicon.ico" />
         <script async src="https://cdn.splitbee.io/sb.js"></script>
+        <meta property="og:title" content="Fanvest - Fund your film with crypto" />
+<meta property="og:type" content="website" />
+<meta property="og:image" content="https://res.cloudinary.com/dev-connect/image/upload/v1638594969/img/ogtitle_y055c2.svg" />
+<meta property="og:url" content="https://fanvest.in" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@fanvest" />
+<meta name="twitter:title" content="Fanvest" />
+<meta name="twitter:description" content="Fund your film with crypto" />
+<meta name="twitter:image" content="https://res.cloudinary.com/dev-connect/image/upload/v1638594969/img/ogtitle_y055c2.svg" />
       </Head>
       <Heading
         letterSpacing="3%"
